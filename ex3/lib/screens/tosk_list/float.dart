ec602576fe05f8d998f796b71c4995636ba84ac9
Caesar <sirostaib@gmail.com>
2021-11-24 07:50:22 +0300

    added float for task finished float.dart in tosk_list folder which is mainly the two floating buttons for update and cancel.

---
+++ ex3/lib/screens/tosk_list/float.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:ex3/models/todo.dart';
+
+class TaFloat extends StatelessWidget {
+  Todo T;
+  TaFloat(this.T);
+
+  // these are the two floating Action Buttons for "Update" and "Cancel"
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        FloatingActionButton.extended(
+          heroTag: null,
+          onPressed: () {
+            Navigator.pop(context, Todo);
+          },
+          label: const Text("Update"),
+          icon: const Icon(Icons.check_circle),
+        ),
+        FloatingActionButton.extended(
+          heroTag: null,
+          onPressed: () {
+            Navigator.pop(context, null);
+          },
+          label: const Text("Cancel"),
+          icon: const Icon(Icons.cancel),
+        ),
+      ],
+    );
+  }
+}
